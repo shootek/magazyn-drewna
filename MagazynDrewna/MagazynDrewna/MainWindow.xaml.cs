b0398fc99c7facbe7698d979368092436567f066
shootek <167685606+shootek@users.noreply.github.com>
2026-03-14 12:21:18 +0100

    Add files via upload

---
+++ MagazynDrewna/MagazynDrewna/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MagazynDrewna
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadTestData();
+        }
+    private void LoadTestData()
+        {
+            var woods = new List<Wood>()
+            {
+                new Wood{ Id=1, Nazwa="Sosna", Gatunek="Iglaste", Dlugosc=2.5, Ilosc=30, Lokalizacja="Magazyn A"},
+                new Wood{ Id=2, Nazwa="Dąb", Gatunek="Liściaste", Dlugosc=3.0, Ilosc=15, Lokalizacja="Magazyn B"},
+                new Wood{ Id=3, Nazwa="Świerk", Gatunek="Iglaste", Dlugosc=2.0, Ilosc=50, Lokalizacja="Magazyn C"},
+                new Wood{ Id=4, Nazwa="Buk", Gatunek="Liściaste", Dlugosc=3.0, Ilosc=20, Lokalizacja="Magazyn D"},
+
+            };
+
+            WoodGrid.ItemsSource = woods;
+        }
+    }
+}
---
+++ MagazynDrewna/MagazynDrewna/Wood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazynDrewna
+{
+    public class Wood
+    {
+        public int Id { get; set; }
+        public string Nazwa {  get; set; }
+        public string Gatunek { get; set; }
+        public double Dlugosc {  get; set; }
+        public int Ilosc { get; set; }
+        public string Lokalizacja { get; set; }
+
+    }
+}
